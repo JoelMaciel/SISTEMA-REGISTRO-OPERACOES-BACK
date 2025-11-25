56cef70ad3e3a0ccf478a56cb0ba642872a12b74
Joel <jmvina37@example.com>
2025-11-25 14:44:45 -0300

    fiz: reafatorando adicionar arma a uma ocorrencia

--- src/modules/ocorrencia/application/usecase/arma/add-arma.ts
+++ src/modules/ocorrencia/application/usecase/arma/add-arma.ts
@@ -1,6 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { IOcorrenciaRepository } from 'src/modules/ocorrencia/infra/repository/interfaces/IOcorrenciaRepository';
-
 import { Arma } from 'src/modules/ocorrencia/domain/entities/arma';
 import { ArmaRequestDTO } from '../../dto/schema/CreateOcorrenciaSchema';
 import { ArmaResponseDTO } from '../../dto/response/ArmaResponseDTO';
@@ -17,9 +16,7 @@ export class AddArmaToOcorrenciaUseCase {
     ocorrenciaId: string,
     dto: ArmaRequestDTO,
   ): Promise<ArmaResponseDTO> {
-    const ocorrencia = await this.ocorrenciaRepository.findById(ocorrenciaId, [
-      'armas',
-    ]);
+    const ocorrencia = await this.ocorrenciaRepository.findById(ocorrenciaId);
 
     if (!ocorrencia) {
       throw new AppError(`Ocorrência ${ocorrenciaId} não encontrada.`, 404);
@@ -30,26 +27,11 @@ export class AddArmaToOcorrenciaUseCase {
     novaArma.calibre = dto.calibre;
     novaArma.numeracao = dto.numeracao;
     novaArma.capacidade = dto.capacidade;
+
     novaArma.ocorrencia = ocorrencia;
 
-    ocorrencia.armas.push(novaArma);
+    const armaSalva = await this.ocorrenciaRepository.saveArma(novaArma);
 
-    const ocorrenciaAtualizada = await this.ocorrenciaRepository.save(
-      ocorrencia,
-    );
-
-    const salvo = ocorrenciaAtualizada.armas.find(
-      (a) =>
-        a.tipo === novaArma.tipo &&
-        a.calibre === novaArma.calibre &&
-        a.capacidade === novaArma.capacidade &&
-        a.numeracao === novaArma.numeracao,
-    );
-
-    if (!salvo) {
-      throw new AppError('Falha ao criar a arma');
-    }
-
-    return new ArmaResponseDTO(salvo);
+    return new ArmaResponseDTO(armaSalva);
   }
 }
