ecc0f52e3cf495433b73657eed8209ee77fc8cad
Joel <jmvina37@example.com>
2025-12-15 13:57:37 -0300

    fix: deletando etidades desnecessarias

--- src/modules/fiscal/domain/enums/local-atividade.enum.ts
+++
@@ -1,9 +0,0 @@
-export enum LocalAtividade {
-  PIRAMBU = 'Pirambu',
-  CRISTO_REDENTOR = 'Cristo Redentor',
-  BARRA_DO_CEARA = 'Barra Do Ceará',
-  CARLITO_PAMPLONA = 'Carlito Pamplona',
-  ALVARO_WEYNE = 'Álvaro Weyne',
-  JACARECANGA = 'Jacarecanga',
-  COLONIA = 'Colônia',
-}
